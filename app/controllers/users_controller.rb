16c743a55a8a4f8b63627be04924310f75a519e8
nina07 <niyanta150792@gmail.com>
2016-06-09 20:53:04 +0530

    Edit via AJAX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      byebug
       redirect_to @user
     else
       render 'new'
@@ -17,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-      @blogs = @user.blogs
+    @blogs = @user.blogs
   end
 
   def update
